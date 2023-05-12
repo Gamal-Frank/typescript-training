0bfd2e8b5d9e0f54f94fd2f420a0dd987f4e10f7
frank <gamalgamer10@yahoo.com>
2023-05-12 10:30:00 +0300

    added functions

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 type stringOrnumber = string|number
+type stringOrnumberArray= (string|number)[]
 type Guitarist={
     name?:string,
     active:boolean,
-    albums:(string|number)[]
+    albums:stringOrnumberArray
 }
+
+const add =(a:number,b:number)=>{return a+b}
+const logMsg=(message:any)=>{console.log(message);}
+logMsg(add(2,1))
